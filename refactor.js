b300547f7aa2960b7a29537b828265dd343d2f8c
Erik <sanchezerik78@yahoo.com>
2016-04-04 18:13:40 -0400

    second

--- refactor.js
+++ refactor.js
@@ -1,47 +1,62 @@
 (function(){
   'use strict'
 
-////1
+/////////////////////////////////////1
 var one = function doSomethingCool() {
   console.log("Something Cool!");
 }
-////
+/////////////////////////////////////
 
-////2
+
+/////////////////////////////////////2
 setTimeOut(function() {
   alert("Hello, World!");
 }, 2000);
-////
+/////////////////////////////////////
 
-////3
+/////////////////////////////////////3
 //[c]: z then y
 //z doesn't have a set time. y has a set time of 1 second
-////
+/////////////////////////////////////
 
-////4
+/////////////////////////////////////4
 var reverseStr = function reverse(str) {
   return str.split('').reverse().join('');
 }
-////
+/////////////////////////////////////
 
-////5
+/////////////////////////////////////5
+var spanishColor = function(colorName) {
+  var colors = {rojo: "#ff0000", blanco: "#ffffff", azul: "#0000ff", verde: "#00ff00", negro: "#000000"};
+  return colors[colorName];
+}
+/////////////////////////////////////
 
-////
-
-////6
+/////////////////////////////////////6
 var foo;
 foo = "bar";
-////
+/////////////////////////////////////
 
-////7
+/////////////////////////////////////7
+var callNtimes = function(callback, n) {
+  var range = _.range(n);
+  _.each(range, callback);
+};
+/////////////////////////////////////
 
-////
+/////////////////////////////////////8
+var score = 0;
 
-////8
+var increaseScore = function() {
+  score++;
+};
 
-////
+var decreaseScore = function() {
+  score--;
+};
+/////////////////////////////////////
 
-////9
+/////////////////////////////////////9
 var addNumbers = function(numberA, numberB) {
   return numberA + numberB;
 };
@@ -49,18 +64,27 @@ var addNumbers = function(numberA, numberB) {
 var twoPlusTwo = addNumbers(2, 2);
 
 console.log(twoPlusTwo);
-////
+/////////////////////////////////////
 
-////10
+/////////////////////////////////////10
+var speed = 0;
 
-////
+var accelerate = function(amount){
+  if (amount == undefined){
+    amount = 1;
+    return speed += amount;
+  }else{
+    return speed += amount;
+  }
+}
+/////////////////////////////////////
 
 
 
 
 
 
-////////bonus
+/////////////////////////////////////bonus
   var callLater = function(timeout, callback) {
     var defaultTimeout =  1000;
 
